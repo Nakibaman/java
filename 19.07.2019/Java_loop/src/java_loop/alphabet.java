03378332bddbf515bd4881d348db9b087776e2b2
Nakibaman <nakibaman@gmail.com>
2019-07-19 15:55:00 +0600

    up-2v1
    up-2v1

--- 19.07.2019/Java_loop/src/java_loop/alphabet.java
+++ 19.07.2019/Java_loop/src/java_loop/alphabet.java
@@ -9,6 +9,8 @@ public class alphabet {
         System.out.printf("Enter a charecter");
         Scanner o=new Scanner(System.in);
         c=o.next().charAt(0);
+        if(c>='0'&&c<='9')
+            System.out.printf("Number");
         //0-9;A-Z;a-z;$#@
         //Number;Alphabet;Special charecter
     }
