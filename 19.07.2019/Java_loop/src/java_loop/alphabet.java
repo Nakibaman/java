ebf9db89c10fa832e34e1d0978253df01679762e
Nakibaman <nakibaman@gmail.com>
2019-07-19 16:07:45 +0600

    up-2V2
    up-2v2

--- 19.07.2019/Java_loop/src/java_loop/alphabet.java
+++ 19.07.2019/Java_loop/src/java_loop/alphabet.java
@@ -11,6 +11,12 @@ public class alphabet {
         c=o.next().charAt(0);
         if(c>='0'&&c<='9')
             System.out.printf("Number");
+        else if(c>='A' && c<='Z')
+            System.out.printf("Alphabet");
+        else if(c>='a' && c<='z')
+            System.out.printf("Alphabet");
+        else
+            System.out.printf("Special Charecter");
         //0-9;A-Z;a-z;$#@
         //Number;Alphabet;Special charecter
     }
