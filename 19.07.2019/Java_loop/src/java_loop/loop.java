0e81c31237812bbb3c2894b6212c9498e6733380
Nakibaman <nakibaman@gmail.com>
2019-07-19 17:42:16 +0600

    up-5
    up-5

---
+++ 19.07.2019/Java_loop/src/java_loop/loop.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_loop;
+
+public class loop {
+    public static void main(String[] args) {
+        int i;
+        for(i=100;i>=1;i=i-1)
+            System.out.printf("%d ",i);
+        
+    }
+}
