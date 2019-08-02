7b1771130f177eaa1d46c19aa93c0715ace44054
Nakibaman <nakibaman@gmail.com>
2019-08-02 15:18:37 +0600

    u1
    u1

---
+++ 02.08.2019/FullPyramid/src/fullpyramid/FullPyramid.java
@@ -0,0 +1,18 @@
+
+package fullpyramid;
+
+public class FullPyramid {
+
+    public static void main(String[] args) {
+        int i,j;
+        for(i=1;i<=4;i++)
+        {
+            for(j=4;j>i;j--)
+            {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+    
+}
