626d0a6acd79b4b70b333a87af00e13cdd5d3e75
Nakibaman <nakibaman@gmail.com>
2019-09-13 15:39:46 +0600

    u-2
    u-2

--- 13.09.2019/Object_Oriented/src/object_oriented/Object_Oriented.java
+++ 13.09.2019/Object_Oriented/src/object_oriented/Object_Oriented.java
@@ -8,11 +8,7 @@ public class Object_Oriented {
    
     public static void main(String[] args) {
         A ob=new A();
-        ob.id=182313002;
-        ob.name="Raihanur Rahman";
-        ob.age=27;
-        ob.cont_number=01700000000;
-        
+        ob.get_info(182313002, "Raihanur Rahman", 27, 01700000000);
         System.out.println(ob.name);
         System.out.println(ob.id);
         
