6c2d2cd2642e70952909b6c8bb4c58e52c7587d9
Nakibaman <nakibaman@gmail.com>
2019-09-13 16:02:09 +0600

    u-5
    u-5

--- 13.09.2019/Object_Oriented/src/object_oriented/A.java
+++ 13.09.2019/Object_Oriented/src/object_oriented/A.java
@@ -2,10 +2,10 @@
 package object_oriented;
 
 public class A {
-    public int id;
-    public String name;
-    public int age;
-    public int cont_number;
+    private int id;
+    private String name;
+    private int age;
+    private int cont_number;
     public void get_info(int a,String b,int c,int d)
     {
         id=a;
--- 13.09.2019/Object_Oriented/src/object_oriented/Object_Oriented.java
+++ 13.09.2019/Object_Oriented/src/object_oriented/Object_Oriented.java
@@ -9,8 +9,11 @@ public class Object_Oriented {
     public static void main(String[] args) {
         A ob=new A();
         ob.get_info(182313002, "Raihanur Rahman", 27, 01700000000);
-        System.out.println(ob.name);
-        System.out.println(ob.id);
+        ob.show_info();
+        
+        A ob2=new A();
+        ob2.get_info(182313003, "Asif Mahamud", 26, 01711111111);
+        ob2.show_info();
         
     }
     
